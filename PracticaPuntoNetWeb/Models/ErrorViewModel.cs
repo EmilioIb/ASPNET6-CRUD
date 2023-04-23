3803328ad5cb5ed42ead54f703e7f6f1969fe2e9
Emilio Ibarra <emilio.ibarra.270@gmail.com>
2023-04-23 11:41:03 -0600

    Agregar archivos de proyecto.

---
+++ PracticaPuntoNetWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PracticaPuntoNetWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
